504962a7dcf9d07162d19bd5d050adb826b2a88b
hsjung22 <hsjung22@gmail.com>
2016-12-06 21:13:45 -0800

    template pattern report example

---
+++ ch_3_template/report.rb
@@ -0,0 +1,41 @@
+class Report
+
+	def initialize
+		@title = 'Monthly Report'
+		@text = ['Things are going', 'well']
+	end
+
+	def output_report
+		output_start
+		output_head
+		output_body_start
+		output_body
+		output_body_end
+		output_end
+	end
+
+	def output_body
+		@text.each { |line| output_line(line) }
+	end
+
+	def output_line(line)
+		raise 'Called abstract method: output_line'
+	end
+
+	def output_start
+	end
+
+	def output_head
+		raise 'Called abstract method: output_head'
+	end
+
+	def output_body_start
+	end
+
+	def output_body_end
+	end
+
+	def output_end
+	end	
+
+end
