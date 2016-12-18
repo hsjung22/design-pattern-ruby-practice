484d69c87454deb1d4037d5d0c2f6cc16cd87e7e
hsjung22 <hsjung22@gmail.com>
2016-12-17 18:02:11 -0800

    virtual proxy

---
+++ ch_10_proxy/virtual_account_proxy.rb
@@ -0,0 +1,29 @@
+class VirtualAccountProxy
+	
+	def initialize(&creation_block)
+		@creation_block = creation_block
+	end
+
+	def deposit(amount)
+		s = subject
+		return s.deposit(amount)
+	end
+
+	def withdraw(amount)
+		s = subject
+		return s.withdraw(amount)
+	end
+
+	def balance
+		s = subject
+		return s.balance
+	end
+
+	def subject
+		@subject || (@subject = @creation_block.call)
+	end
+
+end
+
+
+# account = VirtualAccountProxy.new {BankAccount.new(100)}
